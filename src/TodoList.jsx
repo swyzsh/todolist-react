648149626d04617d75f1e4ceb1cadc4434ce6eb1
Swyzsh <swyzsh@protonmail.com>
2024-08-11 18:03:10 +0530

    add task logic, add mobile support

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -11,19 +11,31 @@ function TodoList() {
   }
 
   function addTask() {
-
+    if (newTask.trim() !== "") {
+      setTasks((prevTasks) => [ ...prevTasks, newTask ]);
+      setNewTask("");
+    }
   }
 
   function deleteTask(index) {
-
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
 
   function moveTaskUp(index) {
-
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index) {
-
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
   return (
